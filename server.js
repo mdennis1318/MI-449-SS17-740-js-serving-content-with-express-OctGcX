3e9d7120001fa306318383454007121d7978ed9e
Matt Dennis <dennis57@msu.edu>
2017-04-25 20:39:43 -0400

    made all team pages render off one ejs file also cleaned up data for each team in server.js

--- server.js
+++ server.js
@@ -33,37 +33,36 @@ var realmadrid = {
   logo: '/images/Real_Madrid.png',
   league: 'La Liga',
   stadium: 'Santiago Bernabéu Stadium',
-  coach: 'Zinedine Zidane'
-}
-
-var realTrophies = {
-  laliga: {
-    name: 'La Liga: ',
-    wins: '32'
-  },
-  ucl: {
-    name: 'European Cup / UEFA Champions League: ',
-    wins: '11'
-  },
-  eurocup: {
-    name: 'UEFA Cup / UEFA Europa League: ',
-    wins: '2'
-  },
-  copadelrey: {
-    name: 'Copa del Rey: ',
-    wins: '19'
-  },
-  supercopa: {
-    name: 'Supercopa de Espana: ',
-    wins: '9'
-  },
-  supercup: {
-    name: 'UEFA Super Cup: ',
-    wins: '3'
-  },
-  clubwc: {
-    name: 'FIFA Club World Cup: ',
-    wins: '2'
+  coach: 'Zinedine Zidane',
+  trophies: {
+    laliga: {
+      name: 'La Liga: ',
+      wins: '32'
+    },
+    ucl: {
+      name: 'European Cup / UEFA Champions League: ',
+      wins: '11'
+    },
+    eurocup: {
+      name: 'UEFA Cup / UEFA Europa League: ',
+      wins: '2'
+    },
+    copadelrey: {
+      name: 'Copa del Rey: ',
+      wins: '19'
+    },
+    supercopa: {
+      name: 'Supercopa de Espana: ',
+      wins: '9'
+    },
+    supercup: {
+      name: 'UEFA Super Cup: ',
+      wins: '3'
+    },
+    clubwc: {
+      name: 'FIFA Club World Cup: ',
+      wins: '2'
+    }
   }
 }
 
@@ -73,33 +72,32 @@ var barcelona = {
   logo: '/images/barcelonalogo.png',
   league: 'La Liga',
   stadium: 'Camp Nou',
-  coach: 'Luis Enrique'
-}
-
-var barcaTrophies = {
-  laliga: {
-    name: 'La Liga: ',
-    wins: '24'
-  },
-  ucl: {
-    name: 'European Cup / UEFA Champions League: ',
-    wins: '5'
-  },
-  copadelrey: {
-    name: 'Copa del Rey: ',
-    wins: '28'
-  },
-  supercopa: {
-    name: 'Supercopa de Espana: ',
-    wins: '12'
-  },
-  supercup: {
-    name: 'UEFA Super Cup: ',
-    wins: '5'
-  },
-  clubwc: {
-    name: 'FIFA Club World Cup: ',
-    wins: '3'
+  coach: 'Luis Enrique',
+  trophies: {
+    laliga: {
+      name: 'La Liga: ',
+      wins: '24'
+    },
+    ucl: {
+      name: 'European Cup / UEFA Champions League: ',
+      wins: '5'
+    },
+    copadelrey: {
+      name: 'Copa del Rey: ',
+      wins: '28'
+    },
+    supercopa: {
+      name: 'Supercopa de Espana: ',
+      wins: '12'
+    },
+    supercup: {
+      name: 'UEFA Super Cup: ',
+      wins: '5'
+    },
+    clubwc: {
+      name: 'FIFA Club World Cup: ',
+      wins: '3'
+    }
   }
 }
 
@@ -109,33 +107,32 @@ var chelsea = {
   logo: '/images/chelsealogo.png',
   league: 'Barclay\'s Premier League',
   stadium: 'Stamford Bridge',
-  coach: 'Antonio Conte'
-}
-
-var chelTrophies = {
-  premierleague: {
-    name: 'Premier League: ',
-    wins: '5'
-  },
-  ucl: {
-    name: 'European Cup / UEFA Champions League: ',
-    wins: '1'
-  },
-  eurocup: {
-    name: 'UEFA Europa League: ',
-    wins: '1'
-  },
-  leaguecup: {
-    name: 'League Cup: ',
-    wins: '5'
-  },
-  facup: {
-    name: 'FA Cup: ',
-    wins: '7'
-  },
-  supercup: {
-    name: 'UEFA Super Cup: ',
-    wins: '1'
+  coach: 'Antonio Conte',
+  trophies: {
+    premierleague: {
+      name: 'Premier League: ',
+      wins: '5'
+    },
+    ucl: {
+      name: 'European Cup / UEFA Champions League: ',
+      wins: '1'
+    },
+    eurocup: {
+      name: 'UEFA Europa League: ',
+      wins: '1'
+    },
+    leaguecup: {
+      name: 'League Cup: ',
+      wins: '5'
+    },
+    facup: {
+      name: 'FA Cup: ',
+      wins: '7'
+    },
+    supercup: {
+      name: 'UEFA Super Cup: ',
+      wins: '1'
+    }
   }
 }
 
@@ -145,33 +142,32 @@ var juventus = {
   logo: '/images/juventuslogo.png',
   league: 'Serie A',
   stadium: 'Juventus Stadium',
-  coach: 'Massimiliano Allegri'
-}
-
-var juveTrophies = {
-  seriea: {
-    name: 'Serie A: ',
-    wins: '32'
-  },
-  ucl: {
-    name: 'European Cup / UEFA Champions League: ',
-    wins: '2'
-  },
-  eurocup: {
-    name: 'UEFA Europa League / UEFA Cup: ',
-    wins: '1'
-  },
-  copaita: {
-    name: 'Coppa Italia: ',
-    wins: '11'
-  },
-  supercupita: {
-    name: 'Supercoppa Italia: ',
-    wins: '7'
-  },
-  supercup: {
-    name: 'UEFA Super Cup: ',
-    wins: '2'
+  coach: 'Massimiliano Allegri',
+  trophies: {
+    seriea: {
+      name: 'Serie A: ',
+      wins: '32'
+    },
+    ucl: {
+      name: 'European Cup / UEFA Champions League: ',
+      wins: '2'
+    },
+    eurocup: {
+      name: 'UEFA Europa League / UEFA Cup: ',
+      wins: '1'
+    },
+    copaita: {
+      name: 'Coppa Italia: ',
+      wins: '11'
+    },
+    supercupita: {
+      name: 'Supercoppa Italia: ',
+      wins: '7'
+    },
+    supercup: {
+      name: 'UEFA Super Cup: ',
+      wins: '2'
+    }
   }
 }
 
@@ -181,31 +177,31 @@ app.get('/', function (request, response) {
   })
 })
 app.get('/realmadrid', function (request, response) {
-  response.render('pages/realmadrid', {
+  response.render('pages/team', {
     navs: navs,
     teaminfo: realmadrid,
-    trophies: realTrophies
+    trophies: realmadrid.trophies
   })
 })
 app.get('/barcelona', function (request, response) {
-  response.render('pages/barcelona', {
+  response.render('pages/team', {
     navs: navs,
     teaminfo: barcelona,
-    trophies: barcaTrophies
+    trophies: barcelona.trophies
   })
 })
 app.get('/chelsea', function (request, response) {
-  response.render('pages/chelsea', {
+  response.render('pages/team', {
     navs: navs,
     teaminfo: chelsea,
-    trophies: chelTrophies
+    trophies: chelsea.trophies
   })
 })
 app.get('/juventus', function (request, response) {
-  response.render('pages/juventus', {
+  response.render('pages/team', {
     navs: navs,
     teaminfo: juventus,
-    trophies: juveTrophies
+    trophies: juventus.trophies
   })
 })
 
